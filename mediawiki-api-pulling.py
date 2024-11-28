8988b9098e0450f8e43bcd96d214ec3bc2448550
jasonzehongwu <jasonzehongwu@gmail.com>
2024-11-27 21:25:42 -0500

    feat: added function to break the talk page into topic files

--- mediawiki-api-pulling.py
+++ mediawiki-api-pulling.py
@@ -9,6 +9,14 @@ from bs4 import BeautifulSoup
 
 API_ENDPOINT = WIKI_URL + "/w/api.php"
 SCRAPED_FILES_PATH = "pages/"
+NOTES_PATH = "pages/notes/"
+
+replace_keys_dict = {
+    '/': '-',
+    ':': '-',
+    '?': '',
+    '\"': '\''
+}
 
 def get_pages_by_category(api_endpoint, category):
     """
@@ -47,20 +55,29 @@ def scrape_one_page(pagelink, pagename, highlight_sections=False):
                 #print(paratext)
                 if start_writing: f.write('\n\n')
                 if highlight_sections and para.name == "h3":
-                    f.write("=="+paratext.replace('\n\n', '\n')+"==")
-                else:
-                    f.write(paratext.replace('\n\n', '\n'))
+                    f.write("=\n")
+                f.write(paratext.replace('\n\n', '\n'))
                 start_writing = True
         #if para.find(['li']) is not None: # tere is a sublist; don't print it
             #print(para.decompose().get_text().strip())
     f.close()
     #print(f"finished scraping text from page: {pagename}")
 
-replace_keys_dict = {
-    '/': '-',
-    ':': '-',
-    '?': ''
-}
+def parse_discussions(cat_talk_route, notes_path):
+    f = open(cat_talk_route, 'r')
+    topics = f.read().split("=")
+    for topic in topics: #write each topic into separate txtfile
+        paragraphs = topic.split('\n\n')
+        header = paragraphs[0]
+        for key in replace_keys_dict:
+            header = header.replace(key, replace_keys_dict[key])
+        f = open(notes_path+header[1:]+".txt", 'w') # skips bad first char
+        filestring = "" # write everything to here, then clean up before posting
+        for paragraph in paragraphs:
+            filestring += paragraph
+            filestring += '\n\n'
+        f.write(filestring.strip())
+        f.close()
 
 main_cat_json = get_pages_by_category(API_ENDPOINT, MAIN_CATEGORY)
 for page in main_cat_json["query"]["categorymembers"]:
@@ -71,4 +88,5 @@ for page in main_cat_json["query"]["categorymembers"]:
         pagename = pagename.replace(key, replace_keys_dict[key])
     #scrape_one_page(pagelink, SCRAPED_FILES_PATH+pagename)
 #print("scraping the talk page")
-scrape_one_page(DISCUSSION_PAGE, SCRAPED_FILES_PATH+"Category talk-Five Epochs of War and Peace", highlight_sections=True)
+scrape_one_page(DISCUSSION_PAGE, SCRAPED_FILES_PATH+CAT_TALK_FILENAME, highlight_sections=True)
+parse_discussions(SCRAPED_FILES_PATH+CAT_TALK_FILENAME+".txt", NOTES_PATH)
