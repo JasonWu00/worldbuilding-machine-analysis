d3007eeee2eb9d2d0d8185e1676ba57f8e117151
jasonzehongwu <jasonzehongwu@gmail.com>
2025-05-19 00:49:06 -0400

    junk: various testing work

--- sandbox.py
+++ sandbox.py
@@ -4,17 +4,14 @@ For testing out short snips of code.
 import secret_variables
 import mediawiki_api_calls
 import pandas as pd
+import regex_cleaners
 
-pagesize = mediawiki_api_calls.get_pagesize(310)
-print(mediawiki_api_calls.get_revision_history(310))
-print(pagesize)
-print(mediawiki_api_calls.get_revision_deets(3278), 427) # expected result: 427
+# pagesize = mediawiki_api_calls.get_pagesize(310)
+# print(mediawiki_api_calls.get_revision_history(310))
+# print(pagesize)
+# print(mediawiki_api_calls.get_revision_deets(3278), 427) # expected result: 427
+wikitext = mediawiki_api_calls.get_wikitext(3397)
+print(wikitext)
 # hist = mediawiki_api_calls.get_revision_history(583)
 
-# revid_list = []
-# for rev in hist:
-#     revid = rev["revid"]
-#     revid_list.append(revid)
-
-# for revid in revid_list:
-#     print(mediawiki_api_calls.get_revision_deets(revid))
+print(regex_cleaners.deformat_infobox_entity(wikitext))
